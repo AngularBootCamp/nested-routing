ee6a1e97da885979d59b998d49dbaa6b7d85ec1b
Lance Finney <lance.finney@oasisdigital.com>
2023-12-15 16:38:50 -0600

    curriculum update

--- src/app/payroll/payroll-search/payroll-search.component.ts
+++ src/app/payroll/payroll-search/payroll-search.component.ts
@@ -8,7 +8,7 @@ import { SearchBoxComponent } from '../../search/search-box.component';
     <article>
       <header>Payroll Search</header>
       <p>Search for Employee Payroll Records</p>
-      <app-search-box></app-search-box>
+      <app-search-box />
     </article>
   `,
   standalone: true,
